7ccbd316b57960de159678722dc9429577285cfb
Adnan Muhaisen <adnanmohaisen609@gmail.com>
2024-09-07 12:22:31 +0300

    add the initial implimentation for the command service

---
+++ MicroBase.CommandService.Api/Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroBase.CommandService.Api.Controllers;
+
+[ApiController]
+[Route("api/command-service/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post()
+    {
+        Console.WriteLine("Post from Command Service");
+
+        return Ok();
+    }
+}
---
+++ MicroBase.CommandService.Api/Program.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Console.WriteLine($"{Assembly.GetExecutingAssembly().FullName} - Running Env is {builder.Environment.EnvironmentName}");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
